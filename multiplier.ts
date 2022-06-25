7204d8f7d8885c42f66923bf6abb1dc0e0258a66
jbresky <joaquinbresky@hotmail.com>
2022-06-25 16:25:45 -0300

    process

--- multiplier.ts
+++ multiplier.ts
@@ -2,5 +2,29 @@ const multiplicator = (a, b, printText) => {
     console.log(printText, a * b);
     
 }
+console.log(process.argv);
 
-multiplicator('2', 4, '2 * 4 is equal to: ')
+const parseArguments = (args): Array<number> => {
+    if(args.length !== 4) throw new Error('Wrong number of arguments')
+    // posición uno y cero corresponden a la compilacion de ts-node y la ejecucion del archivo multiplier respectivamente
+
+    const firstNumber = Number(process.argv[2])
+    const secondNumber = Number(process.argv[3])
+    //los dos numeros de la multiplicacion que se pasan como argumentos son string, se deben volver números
+
+    if(!isNaN(firstNumber) && !isNaN(secondNumber)){
+        return [
+            firstNumber,
+            secondNumber
+        ] 
+        // devuelve un array de numeros
+     }
+     throw new Error('Provided values were not numbers');
+}
+
+const cleanArguments = parseArguments(process.argv)
+
+const a: number = Number(cleanArguments[0])
+const b: number = Number(cleanArguments[1])
+
+multiplicator(a, b, 'a * b is equal to: ')
